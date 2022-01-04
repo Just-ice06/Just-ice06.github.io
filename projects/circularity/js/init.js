678226cc1d0a17cc85fef61b87dbb998406d2997
Just-ice06 <justicelauryn2006@gmail.com>
2022-01-04 02:49:22 +0000

    refactor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -24,13 +24,19 @@ var init = function (window) {
         var circles = [];	// variable to store all circles in one Array
 
         // TODO 2 : Create a function that draws a circle 
+        funtion drawCircle() {
         circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
         physikz.addRandomVelocity(circle, canvas);
         view.addChild(circle);
         circles.push(circle);
+        }
 
         // TODO 3 / 8 : Call the drawCircle() function 
-        var drawCircle(5); = drawCircle + drawCircle + drawCircle + drawCircle + drawCircle
+        drawCircle();
+        drawCircle();
+        drawCircle();
+        drawCircle();
+        drawCircle();
 
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
@@ -43,22 +49,13 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-            physikz.updatePosition( /* Your Bracket Notation HERE */ );
-            physikz.updatePosition( /* Your Bracket Notation HERE */ );
-            physikz.updatePosition( /* Your Bracket Notation HERE */ );
-            physikz.updatePosition( /* Your Bracket Notation HERE */ );
-            physikz.updatePosition( /* Your Bracket Notation HERE */ );
+
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-            game.checkCirclePosition( /* Your Bracket Notation HERE */ );
-            game.checkCirclePosition( /* Your Bracket Notation HERE */ );
-            game.checkCirclePosition( /* Your Bracket Notation HERE */ );
-            game.checkCirclePosition( /* Your Bracket Notation HERE */ );
-            game.checkCirclePosition( /* Your Bracket Notation HERE */ );
+           
 
             // TODO 9 : Iterate over the array
-            for (var i = 0; i < myArray.length; i++) {
-                var eachValue = myArray[i];
+           
             
         }
     
@@ -75,13 +72,11 @@ var init = function (window) {
             }
             
             // TODO 7 : YOUR CODE STARTS HERE //////////////////////
-            game.checkCirclePosition = function(circle) {
-            if ( circle.x > canvas.width ) {
-                circle.x = 0;
-            }
+            
+
 
             // YOUR TODO 7 CODE ENDS HERE //////////////////////////
-        }var rightEdge = circle.x + circle.radius;
+        }
         
         /////////////////////////////////////////////////////////////
         // --- NO CODE BELOW HERE  --- DO NOT REMOVE THIS CODE --- //
